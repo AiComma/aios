dbcf2cd5b9d6ac4f97ba119a3a87adb8ae1f88f4
liting <luz.liting@gmail.com>
2024-10-26 12:09:49 +0800

    style(newtab): add motion

--- src/newtab.tsx
+++ src/newtab.tsx
@@ -1,3 +1,5 @@
+import { BorderTrail } from '@/components/motion/border-trail'
+import { TextEffect } from '@/components/motion/text-effect'
 import bgImage from '../assets/wallpaper-default.jpg'
 import '@/style.css'
 
@@ -9,9 +11,17 @@ function IndexNewTab() {
         backgroundImage: `url('${bgImage}')`,
       }}
     >
-      <div className="flex w-80 flex-col items-center rounded-lg bg-transparent p-4 backdrop-blur-sm hover:shadow-md hover:backdrop-blur">
-        <h1 className="inline-block bg-gradient-to-r from-blue-500 to-green-500 bg-clip-text text-4xl font-bold text-transparent">AI OS</h1>
-        <p className="inline-block bg-gradient-to-r from-blue-500 to-green-500 bg-clip-text text-2xl  text-transparent">An OS with AI first.</p>
+      <div className="relative flex w-80 origin-center flex-col items-center rounded-lg bg-transparent p-6 backdrop-blur hover:shadow-md">
+        <TextEffect as="h1" per="char" className="bg-gradient-to-r from-blue-500 to-green-500 bg-clip-text text-4xl font-bold text-transparent">
+          AI OS
+        </TextEffect>
+        <TextEffect as="h1" per="char" delay={0.3} className="mt-4 bg-gradient-to-r from-blue-500 to-green-500 bg-clip-text text-2xl text-transparent">
+          An OS with AI first.
+        </TextEffect>
+        <BorderTrail
+          className="bg-gradient-to-l from-blue-500 to-green-500"
+          size={120}
+        />
       </div>
     </main>
   )
